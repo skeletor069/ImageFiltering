97883a6db1de0ad4dabb7d2a70a71c26e998f708
TJ <skeletor069@gmail.com>
2018-05-23 22:22:57 +0200

    little refactoring :)

--- CornernessRoundness.m
+++ CornernessRoundness.m
@@ -1,18 +1,18 @@
 function [ W, Q ] = CornernessRoundness( I_x, I_y )
 
 dimension = size(I_x);
-w = ones(5);
-wn = 2;
+wN = ones(5);
+k = 2;
 Q = zeros(dimension);
 W = zeros(dimension);
 
-for i=wn+1:dimension(1)-wn
-    for j = wn+1:dimension(2)-wn
-        Ix = I_x(i-wn:i+wn , j-wn:j+wn);
-        Iy = I_y(i-wn:i+wn , j-wn:j+wn);
-        Ix2 = Ix .^2 .* w;
-        Iy2 = Iy .^2 .* w;
-        Ixy = Iy .* Ix .* w;
+for i=k+1:dimension(1)-k
+    for j = k+1:dimension(2)-k
+        Ix = I_x(i-k:i+k , j-k:j+k);
+        Iy = I_y(i-k:i+k , j-k:j+k);
+        Ix2 = Ix .^2 .* wN;
+        Iy2 = Iy .^2 .* wN;
+        Ixy = Iy .* Ix .* wN;
         M = [sum(sum(Ix2)) , sum(sum(Ixy)) ; sum(sum(Ixy)) , sum(sum(Iy2))];
         Q(i,j) = 4* det(M) / (trace(M)^2);
         W(i,j) = trace(M)/2 - sqrt((trace(M)/2)^2 - det(M));
